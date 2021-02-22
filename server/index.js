63bb660425ccd0a0b279b65d30b3886322b93cd7
RahulJung <rahuljungchauhan@gmail.com>
2021-02-22 00:36:02 -0500

    - Connect frontend with the backend

--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const path = require("path");
 const axios = require("axios");
 
 const port = 8080;
 const app = express();
 
+// Serve the client
+app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.json());
+
+// Example of a get request
 app.get("/", (req, res) => {
   res.send("Hello from the server!!!");
 });
 
+// Listen to port 8080
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
