57c85067b07b368ca02506533d07b6375ef5d6f5
RahulJung <rahuljungchauhan@gmail.com>
2021-02-22 02:12:44 -0500

    - Import Login component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Login from "./Login.jsx";
 
 class App extends React.Component {
   constructor(props) {
@@ -6,7 +7,11 @@ class App extends React.Component {
   }
 
   render() {
-    return <div>Log In page</div>;
+    return (
+      <div>
+        <Login />
+      </div>
+    );
   }
 }
 
