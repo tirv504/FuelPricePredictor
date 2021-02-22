4a56e89b51f5d224dd98888802e8eae3a4b53ac4
RahulJung <rahuljungchauhan@gmail.com>
2021-02-22 02:11:56 -0500

    - Implement conditional rendering for login/register components

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -4,43 +4,56 @@ import Register from "./Register.jsx";
 class Login extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isRegistered: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange() {
+    this.setState({
+      isRegistered: !this.state.isRegistered,
+    });
   }
 
   render() {
-    return (
-      <div>
-        <div className="form">
-          <h2>Admin Login</h2>
-          <div className="innerForm">
-            <div className="userName">
-              <label>User Name</label>
-              <input
-                name="userName"
-                placeholder="Login with your username"
-                value={this.userName}
-                onChange={this.changeHandler}
-              />
-            </div>
-            <div className="pwd">
-              <label>Password</label>
-              <input
-                type="password"
-                name="password"
-                placeholder="Login with your password"
-                value={this.password}
-                onChange={this.changeHandler}
-                required
-              ></input>
-            </div>
+    if (!this.state.isRegistered) {
+      return (
+        <div>
+          <div className="form">
+            <h2>Admin Login</h2>
+            <div className="innerForm">
+              <div className="userName">
+                <label>User Name</label>
+                <input
+                  name="userName"
+                  placeholder="Login with your username"
+                  value={this.userName}
+                  onChange={this.changeHandler}
+                />
+              </div>
+              <div className="pwd">
+                <label>Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Login with your password"
+                  value={this.password}
+                  onChange={this.changeHandler}
+                  required
+                ></input>
+              </div>
 
-            <button className="reviewBtn" onClick={this.getLinks}>
-              Log In
-            </button>
+              <button className="reviewBtn" onClick={this.handleChange}>
+                Register
+              </button>
+            </div>
           </div>
         </div>
-        <Register />
-      </div>
-    );
+      );
+    } else {
+      return <Register />;
+    }
   }
 }
 
