02ee570775e46aa01dfb0cfc34f3ab4a08b584f7
RahulJung <rahuljungchauhan@gmail.com>
2021-02-22 00:15:27 -0500

    - Create a front end

---
+++ client/src/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.jsx";
+
+// render the App component to the DOM
+ReactDOM.render(<App />, document.getElementById("root"));
