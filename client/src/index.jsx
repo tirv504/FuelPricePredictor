17a68ba0ab96ca3b5f9bda87a6a824c4f8ccbb6f
Tray Irving <T-Irv@Trays-MBP.attlocal.net>
2021-02-26 20:41:03 -0600

    setup for gh-pages to show a live version

--- client/src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App.jsx";
-
-// render the App component to the DOM
-ReactDOM.render(<App />, document.getElementById("root"));
